807d6e454e32b3c6e270173ae02f767b20d87f81
HeroAAX <accounts@herooax.com>
2023-08-16 23:09:59 +0200

    Add title fetcher for youtube videos

--- src/core/recommendations.rs
+++ src/core/recommendations.rs
@@ -1,6 +1,5 @@
-use std::collections::LinkedList;
-
-use twitch_irc::message::{PrivmsgMessage, ServerMessage};
+use egui::TextBuffer;
+use twitch_irc::message::PrivmsgMessage;
 
 #[derive(Clone)]
 pub struct RecommendedSong {
@@ -25,6 +24,41 @@ impl RecommendedSong {
             id,
         }
     }
+
+    pub async fn fetch_yt_title(&mut self) {
+        let res = reqwest::get(self.url.as_str()).await;
+
+        let body = &match res {
+            Ok(res) => res.text().await,
+            _ => return,
+        };
+
+        let body = match body {
+            Ok(res) => res,
+            _ => return,
+        };
+
+        let mut body_split = body.split("<title>");
+        body_split.next().unwrap(); // throw away everything before "<title>"
+
+        let body_split = match body_split.next() {
+            Some(res) => res,
+            _ => return,
+        }
+        .split("</title>")
+        .next();
+
+        let mut body_split = match body_split {
+            Some(res) => res,
+            _ => return,
+        };
+
+        for i in (0..body_split.len()).step_by(22) {
+            body_split.insert_text("\n", i);
+        }
+
+        self.title = body_split.to_owned();
+    }
 }
 
 pub struct SongRecommendationSolver {
@@ -40,29 +74,37 @@ impl SongRecommendationSolver {
         }
     }
 
-    pub fn extract_recommendation(&mut self, irc_message: PrivmsgMessage) -> Vec<RecommendedSong> {
+    pub async fn extract_recommendation(
+        &mut self,
+        irc_message: PrivmsgMessage,
+    ) -> Vec<RecommendedSong> {
         let mut out: Vec<RecommendedSong> = vec![];
         let message_split = irc_message.message_text.split_whitespace();
 
         for message in message_split {
             for filter in self.filter_urls.clone() {
                 if message.starts_with(filter.as_str()) {
-                    out.push(self.gen_recommended_song(message, &irc_message));
+                    out.push(self.gen_recommended_song(message, &irc_message).await);
                 }
             }
         }
         out
     }
 
-    fn gen_recommended_song(&mut self, message: &str, irc_msg: &PrivmsgMessage) -> RecommendedSong {
+    async fn gen_recommended_song(
+        &mut self,
+        message: &str,
+        irc_msg: &PrivmsgMessage,
+    ) -> RecommendedSong {
         self.current_id += 1;
         let mut user = irc_msg.sender.name.clone();
 
         if user.capacity() > 30 {
             user = user[0..30].to_owned();
         }
+
         RecommendedSong {
-            title: "YouTube Video".to_owned(),
+            title: "-".to_owned(),
             url: message.to_owned(),
             user,
             id: self.current_id,
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use self::core::recommendations::RecommendedSong;
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use gui::Songwuensche;
+use twitch_irc::message::PrivmsgMessage;
+use twitch_irc::{
+    login::StaticLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
+    TwitchIRCClient,
+};
+
+use crate::core::{
+    recommendations::SongRecommendationSolver, songrecommendationsconfig::load_config,
+};
+use std::sync::mpsc;
 use std::{
     collections::{LinkedList, VecDeque},
     fmt::format,
 };
-use gui::start_gui;
-
-use std::sync::mpsc;
 
 mod core;
 mod gui;
@@ -15,5 +24,57 @@ extern crate rand;
 
 #[tokio::main]
 async fn main() {
-    start_gui();
+    let songwuensche_config = load_config();
+    let songwuensche_clone = songwuensche_config.clone();
+
+    let (sx, tx) = mpsc::channel();
+
+    let config = ClientConfig::default();
+    let sx = sx.clone();
+
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    client.join(songwuensche_clone.channel_id).unwrap();
+
+    let join_handle = tokio::spawn(async move {
+        let solver = &mut SongRecommendationSolver::new(songwuensche_clone.filter_urls);
+        while let Some(message) = incoming_messages.recv().await {
+            match message {
+                ServerMessage::Privmsg(message) => {
+                    println!("Message: {:?}", message);
+                    let mut t = solver.extract_recommendation(message).await;
+
+                    for ele in &mut t {
+                        ele.fetch_yt_title().await;
+                    }
+
+                    sx.send(t).unwrap();
+                }
+                _rest => println!("rest"),
+            }
+        }
+    });
+
+    println!("created twitch listener");
+
+    join_handle.is_finished();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 720.0)),
+        ..Default::default()
+    };
+
+    let eframe = eframe::run_native(
+        "Songwünsche",
+        options,
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(egui::Visuals::dark());
+            Box::new(Songwuensche::new(tx, songwuensche_config))
+        }),
+    );
+    eframe.unwrap();
+
+    println!("closed window");
+    join_handle.await.unwrap();
 }
